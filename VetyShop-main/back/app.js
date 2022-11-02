9a262565d3d3a8377f0014d02488a4c4e87589b5
Melissa <105521312+k0106@users.noreply.github.com>
2022-11-02 14:46:32 -0500

    first commit

---
+++ VetyShop-main/back/app.js
@@ -0,0 +1,20 @@
+const express=require("express");
+const app = express();
+const errorMiddleware= require("./middleware/errors")
+const cookieParser= require("cookie-parser")
+
+//Uso de constantes importadas
+app.use(express.json());
+app.use(cookieParser());
+
+//Importar rutas
+const productos=require("./routes/products")
+const usuarios=require("./routes/auth")
+
+app.use('/api',productos) //Sujeto a decision (ruta del navegador)
+app.use('/api',usuarios)
+
+//MiddleWares para manejar errores
+app.use(errorMiddleware)
+
+module.exports=app
---
+++ VetyShop-main/back/controllers/authController.js
@@ -0,0 +1,127 @@
+const User = require("../models/auth")
+const ErrorHandler= require("../utils/errorHandler")
+const catchAsyncErrors= require("../middleware/catchAsyncErrors");
+const tokenEnviado = require("../utils/jwtToken");
+const sendEmail = require("../utils/sendEmail")
+const crypto = require("crypto")
+
+//Registrar un nuevo usuario /api/usuario/registro
+exports.registroUsuario= catchAsyncErrors(async (req, res, next) =>{
+    const {nombre, email, password} = req.body;
+
+    const user = await User.create({
+        nombre,
+        email,
+        password,
+        avatar:{
+            public_id:"ANd9GcQKZwmqodcPdQUDRt6E5cPERZDWaqy6ITohlQ&usqp",
+            url:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKZwmqodcPdQUDRt6E5cPERZDWaqy6ITohlQ&usqp=CAU"
+        }
+    })
+    tokenEnviado(user,201,res)
+})
+
+
+//Iniciar Sesion - Login
+exports.loginUser = catchAsyncErrors(async(req, res, next)=>{
+    const { email, password} =  req.body;
+
+    //revisar si los campos estan completos
+    if (!email || !password){
+        return next(new ErrorHandler("Por favor ingrese email & Contraseña", 400))
+    }
+
+    //Buscar al usuario en nuestra base de datos
+    const user = await User.findOne({email}).select("+password")
+    if(!user){
+        return next(new ErrorHandler("Email o contraseña invalidos", 401))
+    }
+
+    //Comparar contraseñas, verificar si está bien
+    const contrasenaOK= await user.compararPass(password);
+
+    if (!contrasenaOK){
+        return next(new ErrorHandler("Contraseña invalida",401))
+    }
+
+    tokenEnviado(user,200,res)
+
+})
+
+//Cerrar Sesion (logout)
+exports.logOut = catchAsyncErrors(async(req, res, next)=>{
+    res.cookie("token",null, {
+         expires: new Date(Date.now()),
+         httpOnly: true
+    })
+
+    res.status(200).json({
+        success:true,
+        message: "Cerró sesión"
+    })
+})
+
+//Olvide mi contraseña, recuperar contraseña
+exports.forgotPassword = catchAsyncErrors ( async( req, res, next) =>{
+    const user= await User.findOne({email: req.body.email});
+
+    if (!user){
+        return next(new ErrorHandler("Usuario no se encuentra registrado", 404))
+    }
+    const resetToken= user.genResetPasswordToken();
+    
+    await user.save({validateBeforeSave: false})
+
+    //Crear una url para hacer el reset de la contraseña
+    const resetUrl= `${req.protocol}://${req.get("host")}/api/resetPassword/${resetToken}`;
+
+    const mensaje=`Hola!\n\nTu link para ajustar una nueva contraseña es el 
+    siguiente: \n\n${resetUrl}\n\n
+    Si no solicitaste este link, por favor comunicate con soporte.\n\n Att:\nVetyShop Store`
+
+    try{
+        await sendEmail({
+            email:user.email,
+            subject: "VetyShop Recuperación de la contraseña",
+            mensaje
+        })
+        res.status(200).json({
+            success:true,
+            message: `Correo enviado a: ${user.email}`
+        })
+    }catch(error){
+        user.resetPasswordToken=undefined;
+        user.resetPasswordExpire=undefined;
+
+        await user.save({validateBeforeSave:false});
+        return next(new ErrorHandler(error.message, 500))
+    }
+})
+
+
+//Resetear la contraseña
+exports.resetPassword = catchAsyncErrors(async (req,res,next) =>{
+    //Hash el token que llego con la URl
+    const resetPasswordToken= crypto.createHash("sha256").update(req.params.token).digest("hex")
+    //Buscamos al usuario al que le vamos a resetear la contraseña
+    const user= await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire:{$gt: Date.now()}
+    })
+    //El usuario si esta en la base de datos?
+    if(!user){
+        return next(new ErrorHandler("El token es invalido o ya expiró",400))
+    }
+    //Diligenciamos bien los campos?
+    if(req.body.password!==req.body.confirmPassword){
+        return next(new ErrorHandler("Contraseñas no coinciden",400))
+    }
+
+    //Setear la nueva contraseña
+    user.password= req.body.password;
+    user.resetPasswordToken=undefined;
+    user.resetPasswordExpire=undefined;
+
+    await user.save();
+    tokenEnviado(user, 200, res)
+})
